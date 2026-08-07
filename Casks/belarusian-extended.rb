7ae9e0c0868208200f4fc1a601582375256fca33
Siarhei Karpesh <siarhei@karpesh.com>
2026-08-08 01:42:15 +0200

    v1.10.1

--- Casks/belarusian-extended.rb
+++ Casks/belarusian-extended.rb
@@ -1,6 +1,6 @@
 cask "belarusian-extended" do
-  version "1.10.0"
-  sha256 "8a9203611c33b2a6425465dffe38ef0906cc9ddfa89e983fffe15a65d4e56123"
+  version "1.10.1"
+  sha256 "cb1b137210ba711d3836ed3d8aa6953e5d71a5405ea78346790738d097c3028b"
 
   url "https://github.com/skarpesh/homebrew-belarusian-keyboard/releases/download/v#{version}/Belarusian.bundle.zip"
   name "Belarusian Extended Keyboard Layouts"
